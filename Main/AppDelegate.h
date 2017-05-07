2185bb844474aa6b221b2d23e807a4e31c407dd3
zhangxianjun <1581758003@qq.com>
2017-05-07 22:03:32 +0800

    初始化工程

---
+++ Main/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Main
+//
+//  Created by ZXJ on 2017/5/7.
+//  Copyright © 2017年 maodenden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Main/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Main
+//
+//  Created by ZXJ on 2017/5/7.
+//  Copyright © 2017年 maodenden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
